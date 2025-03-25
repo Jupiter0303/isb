6230d132a2a980b9b9e5053364710c5042680686
Jupiter0303 <165270679+Jupiter0303@users.noreply.github.com>
2025-03-25 23:50:46 +0400

    encryption

--- lab_1/task1/encryption.py
+++ lab_1/task1/encryption.py
@@ -1,9 +1,19 @@
 from math import ceil
 
 
-def encryption(data:str, key:str)->str:
+def encryption(original_data:str, key:str)->str:
+    data = original_data.replace(" ","")
     height = round(len(data)**0.5)
     width = ceil(len(data)/height)
-    data_in_matrix = list( list(data[x*y] for y in range(width)) for x in range(height))
-    if height*width > len(data):
-        data_in_matrix[height-1] += list('_'*(len(data)-height*width) )
+    data_in_matrix = []
+    for x in range(height):
+        data_in_matrix.append([])
+        for y in range(width):
+            index = x * width + y
+            if(index < len(data)):
+                data_in_matrix[x].append(data[index])
+
+            else:
+                data_in_matrix[x].append('_')
+
+    code = list(int(x) for x in key)
