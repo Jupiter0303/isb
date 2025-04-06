8a13df2db57eda52eae6cf4bb8c4d402884eb4e1
Jupiter0303 <165270679+Jupiter0303@users.noreply.github.com>
2025-04-06 18:14:28 +0400

    неудачная попытка написать функцию декондинга - проблема при зацикливании исходного и ключевого алфавитов

--- lab_1/task2/decryption.py
+++ lab_1/task2/decryption.py
@@ -1,24 +1,70 @@
-from constants import stats as original_alphabet
+from constants import stats
 
-def create_dict_encrypted_text(data:str)->dict:
-    sorted_dict_asc = dict()
+
+def create_encrypted_alphabet(data:str)->list:
+    sorted_dict = dict()
     encrypted_alphabet = set(data)
+    data_size = len(data)
     for x in encrypted_alphabet:
-        sorted_dict_asc[x] = data.count(x)/len(data)
-    sorted_dict_asc = dict(sorted(sorted_dict_asc.items(), key=lambda item: item[1], reverse=True))
-    return sorted_dict_asc
+        sorted_dict[x] = data.count(x)/data_size
+    sorted_dict = dict(sorted(sorted_dict.items(), key=lambda item: item[1], reverse=True))
+    return list(sorted_dict.keys())
+
+
+def decoding(data: str, key: dict)->str:
+    size = len(key)
+    keys = list(key.keys())
+    passed_chars = set()
+    for index in range(size):
+        cur_encrypted_char = keys[index]
+        if cur_encrypted_char in passed_chars:
+            continue
+        if key[cur_encrypted_char] in keys:
+            replacement_char = cur_encrypted_char
+            while True:
+                data = data.replace(key[cur_encrypted_char], '&')
+                data = data.replace(replacement_char, key[cur_encrypted_char])
+                passed_chars.add(cur_encrypted_char)
+                cur_encrypted_char = key[cur_encrypted_char]
+
+                if key[cur_encrypted_char] in keys and not cur_encrypted_char in passed_chars:
+                    data = data.replace(key[cur_encrypted_char], '#')
+                    data = data.replace('&', key[cur_encrypted_char])
+                    cur_encrypted_char = key[cur_encrypted_char]
+                else:
+                    break
+
+        else:
+            data = data.replace(cur_encrypted_char,key[cur_encrypted_char])
+            passed_chars.add(cur_encrypted_char)
+def swap(data: str, symbol1: str, symbol2: str) -> str:
+    data.replace(symbol1,'##&')
+    data.replace(symbol2, symbol1)
+    data.replace(symbol1, symbol2)
+    return data
 
 
 def decryption(encrypted_text:str)->str:
-    encrypted_text.replace('\n','')
-    encrypted_alphabet = create_dict_encrypted_text(encrypted_text)
-    encrypted_alphabet_keys = encrypted_alphabet.keys()
-    original_alphabet_keys = original_alphabet.keys()
-    encrypted_text.replace(encrypted_alphabet[encrypted_alphabet_keys[0]],
-                           original_alphabet_keys[0])
-    start1 = 1
-    start2 = 1
-    while(start1 < len(encrypted_alphabet_keys)
-          and start2 < len(original_alphabet_keys)):
-        print(encrypted_text)
-        print (Выберет дей)
+    encrypted_alphabet = create_encrypted_alphabet(encrypted_text)
+    original_alphabet = list(stats.keys())
+    print("Формирование ключа:")
+    key = dict(zip(encrypted_alphabet,original_alphabet))
+    while True:
+        print(f"Текущий текст:\n {encrypted_text}")
+        print(" Поменять символы - 1 \n Завершить - 2" )
+        choice = input().strip()
+        if choice == '1':
+            while True:
+                symbol1 = input("1-ый символ: ").strip()
+                symbol2 = input("2-ой символ: ").strip()
+                if not (symbol1 in original_alphabet and symbol2 in original_alphabet):
+                    print("Некорректно введенный символ, повторите снова.")
+                else:
+                    encrypted_text = swap(encrypted_text,symbol1,symbol2)
+
+                    break
+
+
+
+print(encrypted_text)
+    return encrypted_text
