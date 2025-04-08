679378d9f6c629911c13cd1a4cd5622cdafcc6f4
Jupiter0303 <165270679+Jupiter0303@users.noreply.github.com>
2025-04-08 16:14:22 +0400

    изменена логика составления путь по которому будем смотреть столбцы матрицы в path_generation + pep8

--- lab_1/task1/encryption.py
+++ lab_1/task1/encryption.py
@@ -1,57 +1,75 @@
 from math import ceil
-from constants import COLUMNS
+from constants import COLUMNS, UNNECESSARY_CHARACTERS, EMPTY_CHAR
 
-def path_generation( key: str) -> tuple:
+
+def path_generation(key: str) -> tuple:
+    """
+    генерирует по ключу кортеж из номеров столбцов на основе строки цифр
+    :param key: строка из цифр
+    :return: кортеж из номеров столбцов
+    """
     if not key.isdigit():
         raise ValueError("Ключ должен содержать только цифры")
+
     code = []
-    start = 0
+    used_positions = set()
     n = len(key)
-    while start < n:
-        end = start
-        while end < n and int(key[start:end + 1]) < COLUMNS:
-            end += 1
-        if end == start:
-            start += 1
+    for start in range(n):
+        if start in used_positions:
             continue
-        num = int(key[start:end])
-        code.append(num)
-        start = end
+
+        for end in range(start + 1, n + 1):
+            num = int(key[start:end])
+            if num >= COLUMNS:
+                break
+
+            if num not in code:
+                code.append(num)
+                used_positions.update(range(start, end))
+                break
+
     return tuple(code)
 
 
-def encryption(original_data:str, key:str)->str:
-    data = original_data.replace(" ","")
-    data = data.replace("!","")
-    data = data.replace("?","")
-    data = data.replace("—","")
-    data = data.replace("-","")
-    data = data.replace(".","")
-    data = data.replace(",","")
-    data = data.replace(";","")
-    data = data.replace(":","")
-    data = data.replace("\n","")
-    data = data.replace("(","")
-    data = data.replace(")","")
-    data = data.lower()
+def normalize_text(data: str) -> str:
+    """
+    подготавливает текст к процессу шифрования
+    :param data: исходная строка
+    :return: подготовленная строка
+    """
+    for k in UNNECESSARY_CHARACTERS:
+        data = data.replace(k, UNNECESSARY_CHARACTERS[k])
 
+    return data.lower()
+
+
+def encryption(original_data: str, key: str) -> str:
+    """
+    шифрование текста методом маршрутной транспозиции
+    :param original_data: незашифрованная строка
+    :param key: ключ - строка из цифр, по которому шифруем текст
+    :return: шифротекст
+    """
+    data = normalize_text(original_data)
+    print(data)
     width = COLUMNS
-    height = ceil(len(data)/width)
+    height = ceil(len(data) / width)
     data_in_matrix = []
+
     for x in range(height):
         data_in_matrix.append([])
         for y in range(width):
             index = x * width + y
-            if(index < len(data)):
+            if index < len(data):
                 data_in_matrix[x].append(data[index])
-
             else:
-                data_in_matrix[x].append('_')
+                data_in_matrix[x].append(EMPTY_CHAR)
+
     code = path_generation(key)
-    print(code, width, height)
-    encrypted_data = ''
+    encrypted_data = list()
+
     for x in range(height):
         for y in code:
-            if not data_in_matrix[x][y] == '_':
-                encrypted_data+= data_in_matrix[x][y]
-    return encrypted_data
+            if not data_in_matrix[x][y] == EMPTY_CHAR:
+                encrypted_data.append(data_in_matrix[x][y])
+    return ''.join(encrypted_data)
