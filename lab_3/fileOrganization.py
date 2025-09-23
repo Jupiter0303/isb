5de4f19422e6c00bbdfac2c31c56e3b250d8e5f5
Jupiter0303 <165270679+Jupiter0303@users.noreply.github.com>
2025-09-23 21:41:41 +0400

    отформатировал fileOrganisation.py и добавил исключения

--- lab_3/fileOrganization.py
+++ lab_3/fileOrganization.py
@@ -1,57 +1,144 @@
-from config import PUBLIC_PEM, PRIVATE_PEM
+import os
+
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
 
+from config import PUBLIC_PEM, PRIVATE_PEM
 
-def serialization_public_key(public_key)->None:
-    with open(PUBLIC_PEM, 'wb') as public_out:
-        public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
-             format=serialization.PublicFormat.SubjectPublicKeyInfo))
+
+def serialization_public_key(public_key) -> None:
+    """
+    сериализация публичного ключа
+    :param public_key:
+    :return: None
+    """
+    try:
+        with open(PUBLIC_PEM, 'wb') as public_out:
+            public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                     format=serialization.PublicFormat.SubjectPublicKeyInfo)
+                             )
+    except Exception as ex:
+        print(f"Ошибка при сериализации публичного ключа: {ex}")
+
 
 def deserialization_public_key():
-    with open(PUBLIC_PEM, 'rb') as pem_in:
-        public_bytes = pem_in.read()
-    return load_pem_public_key(public_bytes)
+    """
+    десериализация публичного ключа
+    :return: публичный ключ в байтах
+    """
+    try:
+        with open(PUBLIC_PEM, 'rb') as pem_in:
+            public_bytes = pem_in.read()
+        return load_pem_public_key(public_bytes)
+    except Exception as ex:
+        print(f"Ошибка при десериализации публичного ключа: {ex}")
 
 
-def serialization_private_key(private_key)->None:
-    with open(PRIVATE_PEM, 'wb') as private_out:
-        private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
-              format=serialization.PrivateFormat.TraditionalOpenSSL,
-              encryption_algorithm=serialization.NoEncryption()))
+def serialization_private_key(private_key) -> None:
+    """
+    сериализация приватного ключа
+    :param private_key:  приватный ключ
+    :return: None
+    """
+    try:
+        with open(PRIVATE_PEM, 'wb') as private_out:
+            private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                        format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                        encryption_algorithm=serialization.NoEncryption())
+                              )
+    except Exception as ex:
+        print(f"Ошибка при сериализации приватного ключа: {ex}")
 
 
 def deserialization_private_key():
-    with open(PRIVATE_PEM, 'rb') as pem_in:
-        private_bytes = pem_in.read()
-    return load_pem_private_key(private_bytes, password=None, )
-
+    """
+       десериализация приватного ключа
+       :return: приватный ключ в байтах
+    """
+    try:
+        with open(PRIVATE_PEM, 'rb') as pem_in:
+            private_bytes = pem_in.read()
+        return load_pem_private_key(private_bytes, password=None, )
+    except Exception as ex:
+        print(f"Ошибка при десериализации приватного ключа: {ex}")
 
 
 def save_bytes_to_file(data: bytes, file_path: str) -> None:
-    with open(file_path, 'wb') as f:
-        f.write(data)
+    """
+    сохранения байтов в файле
+    :param data: данные в байтах
+    :param file_path: путь к файлу для сохранения
+    :return: None
+    """
+    try:
+        with open(file_path, 'wb') as f:
+            f.write(data)
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+    except Exception as ex:
+        print(f"Ошибка при сохранении байтов в файл в функции save_bytes_to_file : {ex}")
 
 
 def load_bytes_from_file(file_path: str) -> bytes:
-    with open(file_path, 'rb') as f:
-        return f.read()
-
-import os
+    """
+    считывание байтов из файла
+    :param file_path: путь к файлу
+    :return: байты, содержащиеся в файле
+    """
+    try:
+        with open(file_path, 'rb') as f:
+            return f.read()
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+    except Exception as ex:
+        print(f"Ошибка при выводе байтов из файла в функции load_bytes_from_file: {ex}")
 
 
 def check_file_not_empty(file_path: str) -> bool:
-    if os.path.getsize(file_path) == 0:
-        print(f"Ошибка! Файл - {file_path} - пустой")
-        return False
+    """
+    проверка на наличие данных в файле
+    :param file_path: путь к файлу
+    :return: bool - результат проверки
+    """
+    try:
+        if os.path.getsize(file_path) == 0:
+            print(f"Ошибка! Файл - {file_path} - пустой")
+            return False
 
-    return True
+        return True
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+    except Exception as ex:
+        print(f"Ошибка при проверке файла в функции check_file_not_empty: {ex}")
 
-def read_txt_file(file_path: str)->str:
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
+
+def read_txt_file(file_path: str) -> str:
+    """
+    считывание строки из текстового файла
+    :param file_path: путь к файлу
+    :return: строка, содержащаяся в нем
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+
+    except Exception as ex:
+        print(f"Ошибка при чтении файла в функции read_txt_file: {ex}")
 
 
-def write_file_txt(data: str, file_path: str)->None:
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(data)
+def write_file_txt(data: str, file_path: str) -> None:
+    """
+    запись данных в текстовый файл
+    :param data: строка для записи
+    :param file_path: путь к файлу
+    :return: None
+    """
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(data)
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+    except Exception as ex:
+        print(f"Ошибка при записи данных в файл в функции write_file_txt: {ex}")
