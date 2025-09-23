65406fca3a4d4c494b1969bb1f00a392d38bb286
Jupiter0303 <165270679+Jupiter0303@users.noreply.github.com>
2025-09-23 20:56:03 +0400

    отформатировал config.py и добавил исключение

--- lab_3/config.py
+++ lab_3/config.py
@@ -1,13 +1,17 @@
 import json
 
+try:
+    with open("settings.json", 'r', encoding='utf-8') as js:
+        config = json.load(js)
 
-with open ("settings.json", 'r') as js:
-    config = json.load(js)
+    INITIAL_FILE = config["initial_file"]
+    ENCRYPTED_FILE = config["encrypted_file"]
+    DECRYPTED_FILE = config["decrypted_file"]
+    SYMMETRIC_KEY = config["symmetric_key"]
+    PUBLIC_PEM = config["public_key"]
+    PRIVATE_PEM = config["secret_key"]
+except FileNotFoundError:
+    print("Файл settings.json не найден.")
 
-
-INITIAL_FILE = config["initial_file"]
-ENCRYPTED_FILE = config["encrypted_file"]
-DECRYPTED_FILE = config["decrypted_file"]
-SYMMETRIC_KEY = config["symmetric_key"]
-PUBLIC_PEM = config["public_key"]
-PRIVATE_PEM = config["secret_key"]
+except Exception as ex:
+    print(f"Ошибка при загрузке конфигурационных данных: {ex}")
